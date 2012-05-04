de0d7ddd277e6fed10271bfd1a335e43a79f3c9b
gardym <mgardin@thoughtworks.com>
2012-05-04 21:14:09 +0800

    - Allow for multiple quotes - Add a stub footer

--- app/public/main.js
+++ app/public/main.js
@@ -1,8 +1,22 @@
 require(["jquery"], function($) {
     $(function() {
-			$(".quote").on("click", function() {
-				$("#raptor").show();
-				$(".quote").hide();
-			});
+
+      var raptorClick = function(nextQuote) {
+        $("#raptor").off("click").closest(".container").hide();
+        $(nextQuote).closest(".container").show();
+      };
+
+      var quoteClick = function() {
+        $(this).off("click").closest(".container").hide()
+
+        var nextQuote = $(this).closest(".container").next().find(".quote");
+        nextQuote.on("click", quoteClick);
+
+        $("#raptor").on("click", function() { raptorClick(nextQuote); }).closest(".container").show();
+      }; 
+
+      $(".quote").closest(".container").hide();
+			$(".quote:first").on("click", quoteClick).closest(".container").show();
+
     });
 });

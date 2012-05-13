da5ffc793cd4fdde54a0611ffb15f7195fd29b2f
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-05-13 17:09:59 +1000

    added haml, added shell for about

--- app/app.js
+++ app/app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var Haml = require('haml');
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('app/index.html', 'utf-8'));
+});
+
+app.get('/about', function(request, response) {
+  response.send(Haml.render(fs.readFileSync('app/about.haml', 'utf-8')));
 });
 
 var port = process.env.PORT || 3000;

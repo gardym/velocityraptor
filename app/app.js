7de9c04141fdbf11564dbe67edb8a9d923ffd340
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-07-07 22:08:19 +1000

    added help page; added copy for about page; style changes around footer and text layout

--- app/app.js
+++ app/app.js
@@ -29,6 +29,10 @@ app.get('/about', function(request, response) {
   response.render('about.haml');
 });
 
+app.get('/halp', function(request, response) {
+  response.render('halp.haml');
+});
+
 app.get('/:permalink', function(request, response) {
   quote = quotes.filter(function(e, i, a) { return e.permalink == request.params.permalink });
 
